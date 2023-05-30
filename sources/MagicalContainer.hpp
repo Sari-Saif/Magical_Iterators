07397d990df4049f9e4e899f82e848fea7bdf956
97253 <sygw6457@gmail.com>
2023-05-30 13:01:29 +0300

    start ...

---
+++ sources/MagicalContainer.hpp
@@ -0,0 +1,14 @@
+/**
+ *@author Sari Safe
+ *@since 30.05.23
+ * URL TUTORIAL:
+ * https://en.cppreference.com/w/
+ */
+
+#ifndef MAGICALCONTAINER_HPP
+#define MAGICALCONTAINER_HPP
+namespace ariel
+{
+};
+
+#endif // MAGICALCONTAINER_HPP
